889da1332dcbd128e8adb2d24c86671708b1b65a
David Oke <davidokenyc@gmail.com>
2025-12-26 14:58:29 -0500

    tested spatial library duckdb, added column reference to citibike data sheet

--- citibike_analysis.py
+++ citibike_analysis.py
@@ -3,6 +3,26 @@ from pathlib import Path # we use Path to establish a location for the folder wh
 from matplotlib import pyplot as plt #plotting library to graph info visually
 from enum import Enum #make enumerated keywords (words with numerical values)
 
+column_dict = {
+    "ride_id": "column00",
+    "rideable_type": "column01",
+    "start yyyy=mm-dd hh" : "column02",
+    "start mm" : "column03",
+    "start ss" : "column04",
+    "end yyyy-mm-dd hh" : "column05",
+    "end mm" : "column06",
+    "end ss" : "column07",
+    "start_name" : "column08",
+    "start_ID" : "column09",
+    "end_name" : "column10",
+    "end_ID" : "column11",
+    "start_lat" : "column12",
+    "start_long" : "column13",
+    "end_lat" : "column14",
+    "end_long" : "column15",
+    "rider_type" : "column16",
+}   
+
 
 #TEMPLATE TO FILL IN VARIABLE NAMES BY YEAR
 list_of_years = ["2024", "2025"] #EDIT HERE TO UPDATE THE LIST OF YEARS TO CHECK
@@ -20,6 +40,10 @@ for year in list_of_years: #iterate through the years to create the variable nam
 
 #CREATE DATABASE, WILL BE CLOSED AT THE END OF RUN
 duck_citibike_connect = duckdb.connect(database="citibike.duckdb") 
+
+#INSTALL/LOAD SPATIAL CONNECTION
+duck_citibike_connect.execute("INSTALL spatial;")
+duck_citibike_connect.execute("LOAD spatial;")
 
 
 #CHECK IF PARQUET HAS ALREADY BEEN CREATED, IF NOT:
@@ -60,6 +84,15 @@ for year in list_of_years: #iterate through the years to load the parquet into d
 
 #ACTUAL ANALYSIS BEGINS HERE
 
+#NTA table in DuckDB database
+duck_citibike_connect.execute("""
+    CREATE OR REPLACE TABLE ntas AS
+    SELECT *
+    FROM ST_Read('NTAs/geo_export_bd71e9cd-14bd-435b-99c8-b573a94a1dc8.shp');
+""")
+
+neighborhood_list = duck_citibike_connect.execute("DESCRIBE ntas").fetchall()
+
 for year in list_of_years: #iterate through the years to read/process duckdb tables
     table_name = list_of_tables[year]
 
@@ -77,11 +110,12 @@ for year in list_of_years: #iterate through the years to read/process duckdb tab
         WHERE column01 = 'classic_bike'
     """).fetchone()[0] #take classic bike counts
 
-
-
     print(f"{year}: {total_count:,} total bikes")
     print(f"{year}: {ebike_count:,} e-bikes")
     print(f"{year}: {classicbike_count:,} classic bikes")
+
+
+print(neighborhood_list)
 
 #CLOSE DATABASE
 duck_citibike_connect.close()
