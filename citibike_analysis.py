54bf4c951e4194791915bdefe5a1e762bedc52c8
David Oke <davidokenyc@gmail.com>
2025-12-26 22:50:46 -0500

    added list of nta's, put them in crz/boro related groups, starting to get numbers on bike trips to and from crz

--- citibike_analysis.py
+++ citibike_analysis.py
@@ -3,6 +3,7 @@ from pathlib import Path # we use Path to establish a location for the folder wh
 from matplotlib import pyplot as plt #plotting library to graph info visually
 from enum import Enum #make enumerated keywords (words with numerical values)
 
+#dictionary to match raw column names with legible names column_dict[name] -> column number
 column_dict = {
     "ride_id": "column00",
     "rideable_type": "column01",
@@ -21,6 +22,305 @@ column_dict = {
     "end_nta" : "column14"
 }   
 
+#escape apostrophes to not have them break in SQL query 
+#(many neighborhoods have apostrophes, like randall's island)
+def escape_apostrophes(lst):
+    for i in range(len(lst)):
+        lst[i] = lst[i].replace("'", "''")
+
+
+#list of ntas (long)
+crz_nta_list = [
+    "Financial District-Battery Park City"
+    ,"Tribeca-Civic Center"
+    ,"The Battery-Governors Island-Ellis Island-Liberty Island"
+    ,"SoHo-Little Italy-Hudson Square"
+    ,"Greenwich Village"
+    ,"West Village"
+    ,"Chinatown-Two Bridges"
+    ,"Lower East Side"
+    ,"East Village"
+    ,"Chelsea-Hudson Yards"
+    ,"Hell's Kitchen"
+    ,"Midtown South-Flatiron-Union Square"
+    ,"Midtown-Times Square"
+    ,"Stuyvesant Town-Peter Cooper Village"
+    ,"Gramercy"
+    ,"Murray Hill-Kips Bay"
+    ,"East Midtown-Turtle Bay"
+    ,"United Nations"
+]
+
+other_mnh_nta_list = [
+    "Upper West Side-Lincoln Square"
+    ,"Upper West Side (Central)"
+    ,"Upper West Side-Manhattan Valley"
+    ,"Upper East Side-Lenox Hill-Roosevelt Island"
+    ,"Upper East Side-Carnegie Hill"
+    ,"Upper East Side-Yorkville"
+    ,"Morningside Heights"
+    ,"Manhattanville-West Harlem"
+    ,"Hamilton Heights-Sugar Hill"
+    ,"Harlem (South)"
+    ,"Harlem (North)"
+    ,"East Harlem (South)"
+    ,"East Harlem (North)"
+    ,"Randall's Island"
+    ,"Washington Heights (South)"
+    ,"Washington Heights (North)"
+    ,"Inwood"
+    ,"Highbridge Park"
+    ,"Inwood Hill Park"
+    ,"Central Park"
+]
+
+bk_nta_list = [
+    "Greenpoint"
+    ,"Williamsburg"
+    ,"South Williamsburg"
+    ,"East Williamsburg"
+    ,"Brooklyn Heights"
+    ,"Downtown Brooklyn-DUMBO-Boerum Hill"
+    ,"Fort Greene"
+    ,"Clinton Hill"
+    ,"Brooklyn Navy Yard"
+    ,"Bedford-Stuyvesant (West)"
+    ,"Bedford-Stuyvesant (East)"
+    ,"Bushwick (West)"
+    ,"Bushwick (East)"
+    ,"The Evergreens Cemetery"
+    ,"Cypress Hills"
+    ,"East New York (North)"
+    ,"East New York-New Lots"
+    ,"Spring Creek-Starrett City"
+    ,"East New York-City Line"
+    ,"Highland Park-Cypress Hills Cemeteries (South)"
+    ,"Carroll Gardens-Cobble Hill-Gowanus-Red Hook"
+    ,"Park Slope"
+    ,"Windsor Terrace-South Slope"
+    ,"Sunset Park (West)"
+    ,"Sunset Park (Central)"
+    ,"Green-Wood Cemetery"
+    ,"Prospect Heights"
+    ,"Crown Heights (North)"
+    ,"Lincoln Terrace Park"
+    ,"Crown Heights (South)"
+    ,"Prospect Lefferts Gardens-Wingate"
+    ,"Bay Ridge"
+    ,"Dyker Heights"
+    ,"Fort Hamilton"
+    ,"Dyker Beach Park"
+    ,"Bensonhurst"
+    ,"Bath Beach"
+    ,"Gravesend (West)"
+    ,"Sunset Park (East)-Borough Park (West)"
+    ,"Borough Park"
+    ,"Kensington"
+    ,"Mapleton-Midwood (West)"
+    ,"Gravesend (South)"
+    ,"Coney Island-Sea Gate"
+    ,"Brighton Beach"
+    ,"Calvert Vaux Park"
+    ,"Flatbush"
+    ,"Flatbush (West)-Ditmas Park-Parkville"
+    ,"Midwood"
+    ,"Gravesend (East)-Homecrest"
+    ,"Madison"
+    ,"Sheepshead Bay-Manhattan Beach-Gerritsen Beach"
+    ,"Ocean Hill"
+    ,"Brownsville"
+    ,"East Flatbush-Erasmus"
+    ,"East Flatbush-Farragut"
+    ,"East Flatbush-Rugby"
+    ,"East Flatbush-Remsen Village"
+    ,"Holy Cross Cemetery"
+    ,"Flatlands"
+    ,"Marine Park-Mill Basin-Bergen Beach"
+    ,"Canarsie"
+    ,"Marine Park-Plumb Island"
+    ,"McGuire Fields"
+    ,"Canarsie Park & Pier"
+    ,"Prospect Park"
+    ,"Barren Island-Floyd Bennett Field"
+    ,"Jamaica Bay (West)"
+    ,"Shirley Chisholm State Park"
+]
+
+bronx_nta_list = [
+    "Mott Haven-Port Morris"
+    ,"Melrose"
+    ,"Hunts Point"
+    ,"Longwood"
+    ,"North & South Brother Islands"
+    ,"Morrisania"
+    ,"Claremont Village-Claremont (East)"
+    ,"Crotona Park East"
+    ,"Crotona Park"
+    ,"Concourse-Concourse Village"
+    ,"Highbridge"
+    ,"Mount Eden-Claremont (West)"
+    ,"Yankee Stadium-Macombs Dam Park"
+    ,"Claremont Park"
+    ,"University Heights (South)-Morris Heights"
+    ,"Mount Hope"
+    ,"Fordham Heights"
+    ,"West Farms"
+    ,"Tremont"
+    ,"Belmont"
+    ,"University Heights (North)-Fordham"
+    ,"Bedford Park"
+    ,"Norwood"
+    ,"Kingsbridge Heights-Van Cortlandt Village"
+    ,"Kingsbridge-Marble Hill"
+    ,"Riverdale-Spuyten Duyvil"
+    ,"Soundview-Bruckner-Bronx River"
+    ,"Soundview-Clason Point"
+    ,"Castle Hill-Unionport"
+    ,"Parkchester"
+    ,"Soundview Park"
+    ,"Westchester Square"
+    ,"Throgs Neck-Schuylerville"
+    ,"Pelham Bay-Country Club-City Island"
+    ,"Co-op City"
+    ,"Hart Island"
+    ,"Ferry Point Park-St. Raymond Cemetery"
+    ,"Pelham Parkway-Van Nest"
+    ,"Morris Park"
+    ,"Pelham Gardens"
+    ,"Allerton"
+    ,"Hutchinson Metro Center"
+    ,"Williamsbridge-Olinville"
+    ,"Eastchester-Edenwald-Baychester"
+    ,"Wakefield-Woodlawn"
+    ,"Woodlawn Cemetery"
+    ,"Van Cortlandt Park"
+    ,"Bronx Park"
+    ,"Pelham Bay Park"
+]
+
+queens_nta_list = [
+    "Astoria (North)-Ditmars-Steinway"
+    ,"Old Astoria-Hallets Point"
+    ,"Astoria (Central)"
+    ,"Astoria (East)-Woodside (North)"
+    ,"Queensbridge-Ravenswood-Dutch Kills"
+    ,"Rikers Island"
+    ,"Sunnyside Yards (North)"
+    ,"St. Michael's Cemetery"
+    ,"Astoria Park"
+    ,"Long Island City-Hunters Point"
+    ,"Sunnyside"
+    ,"Woodside"
+    ,"Sunnyside Yards (South)"
+    ,"Calvary & Mount Zion Cemeteries"
+    ,"Jackson Heights"
+    ,"East Elmhurst"
+    ,"North Corona"
+    ,"Elmhurst"
+    ,"Corona"
+    ,"Maspeth"
+    ,"Ridgewood"
+    ,"Glendale"
+    ,"Middle Village"
+    ,"Mount Olivet & All Faiths Cemeteries"
+    ,"Middle Village Cemetery"
+    ,"St. John Cemetery"
+    ,"Highland Park-Cypress Hills Cemeteries (North)"
+    ,"Rego Park"
+    ,"Forest Hills"
+    ,"College Point"
+    ,"Whitestone-Beechhurst"
+    ,"Bay Terrace-Clearview"
+    ,"Murray Hill-Broadway Flushing"
+    ,"East Flushing"
+    ,"Queensboro Hill"
+    ,"Flushing-Willets Point"
+    ,"Fort Totten"
+    ,"Kissena Park"
+    ,"Kew Gardens Hills"
+    ,"Pomonok-Electchester-Hillcrest"
+    ,"Fresh Meadows-Utopia"
+    ,"Jamaica Estates-Holliswood"
+    ,"Jamaica Hills-Briarwood"
+    ,"Mount Hebron & Cedar Grove Cemeteries"
+    ,"Cunningham Park"
+    ,"Kew Gardens"
+    ,"Richmond Hill"
+    ,"South Richmond Hill"
+    ,"Ozone Park (North)"
+    ,"Woodhaven"
+    ,"South Ozone Park"
+    ,"Ozone Park"
+    ,"Howard Beach-Lindenwood"
+    ,"Spring Creek Park"
+    ,"Auburndale"
+    ,"Bayside"
+    ,"Douglaston-Little Neck"
+    ,"Oakland Gardens-Hollis Hills"
+    ,"Alley Pond Park"
+    ,"Jamaica"
+    ,"South Jamaica"
+    ,"Baisley Park"
+    ,"Springfield Gardens (North)-Rochdale Village"
+    ,"St. Albans"
+    ,"Hollis"
+    ,"Glen Oaks-Floral Park-New Hyde Park"
+    ,"Bellerose"
+    ,"Queens Village"
+    ,"Cambria Heights"
+    ,"Laurelton"
+    ,"Springfield Gardens (South)-Brookville"
+    ,"Rosedale"
+    ,"Montefiore Cemetery"
+    ,"Far Rockaway-Bayswater"
+    ,"Rockaway Beach-Arverne-Edgemere"
+    ,"Breezy Point-Belle Harbor-Rockaway Park-Broad Channel"
+    ,"Rockaway Community Park"
+    ,"LaGuardia Airport"
+    ,"Flushing Meadows-Corona Park"
+    ,"Forest Park"
+    ,"John F. Kennedy International Airport"
+    ,"Jamaica Bay (East)"
+    ,"Jacob Riis Park-Fort Tilden-Breezy Point Tip"
+]
+
+si_nta_list = [
+    "St. George-New Brighton"
+    ,"Tompkinsville-Stapleton-Clifton-Fox Hills"
+    ,"Rosebank-Shore Acres-Park Hill"
+    ,"West New Brighton-Silver Lake-Grymes Hill"
+    ,"Westerleigh-Castleton Corners"
+    ,"Port Richmond"
+    ,"Mariner's Harbor-Arlington-Graniteville"
+    ,"Snug Harbor"
+    ,"Grasmere-Arrochar-South Beach-Dongan Hills"
+    ,"New Dorp-Midland Beach"
+    ,"Todt Hill-Emerson Hill-Lighthouse Hill-Manor Heights"
+    ,"New Springville-Willowbrook-Bulls Head-Travis"
+    ,"Freshkills Park (North)"
+    ,"Oakwood-Richmondtown"
+    ,"Great Kills-Eltingville"
+    ,"Arden Heights-Rossville"
+    ,"Annadale-Huguenot-Prince's Bay-Woodrow"
+    ,"Tottenville-Charleston"
+    ,"Freshkills Park (South)"
+    ,"Fort Wadsworth"
+    ,"Hoffman & Swinburne Islands"
+    ,"Miller Field"
+    ,"Great Kills Park"
+]
+
+#apply escape apostrophe function for all borough lists
+escape_apostrophes(crz_nta_list)
+escape_apostrophes(other_mnh_nta_list)
+escape_apostrophes(bk_nta_list)
+escape_apostrophes(bronx_nta_list)
+escape_apostrophes(queens_nta_list)
+escape_apostrophes(si_nta_list)
+
+#make non-mnh and non-cbd lists
+non_mnh_nta_list = si_nta_list + bk_nta_list + bronx_nta_list + queens_nta_list
+non_cbd_nta_list = non_mnh_nta_list + other_mnh_nta_list
 
 #TEMPLATE TO FILL IN VARIABLE NAMES BY YEAR
 list_of_years = ["2024", "2025"] #EDIT HERE TO UPDATE THE LIST OF YEARS TO CHECK
@@ -145,26 +445,28 @@ for year in list_of_years: #iterate through the years to read/process duckdb tab
             );
         """)
 
-    # ebike_count = duck_citibike_connect.execute(f"""
-    #     SELECT COUNT(*) FROM {table_name} 
-    #     WHERE column01 = 'electric_bike'
-    # """).fetchone()[0] #take ebike counts
+    non_cbd_to_cbd = duck_citibike_connect.execute(f"""
+        SELECT COUNT(*) FROM {table_name} 
+        WHERE {column_dict['start_nta']} = ANY(?)
+        AND {column_dict['end_nta']} = ANY(?)
+    """, [non_cbd_nta_list, crz_nta_list]).fetchone()[0] #take counts of rides that started outside of cbd, ended in cbd
 
-    # classicbike_count = duck_citibike_connect.execute(f"""
-    #     SELECT COUNT(*) FROM {table_name} 
-    #     WHERE column01 = 'classic_bike'
-    # """).fetchone()[0] #take classic bike counts
+    cbd_to_non_cbd = duck_citibike_connect.execute(f"""
+        SELECT COUNT(*) FROM {table_name} 
+        WHERE {column_dict['start_nta']} = ANY(?)
+        AND {column_dict['end_nta']} = ANY(?)
+    """, [crz_nta_list, non_cbd_nta_list]).fetchone()[0] #take counts of rides that started in cbd, ended outside of cbd
 
-    # print(f"{year}: {ebike_count:,} e-bikes")
-    # print(f"{year}: {classicbike_count:,} classic bikes")
+    print(f"{year}: {non_cbd_to_cbd:,} trips from non-CRZ to CRZ \n")
+    print(f"{year}: {cbd_to_non_cbd:,} trips from CRZ to non-CRZ \n")
 
 
 # print(neighborhood_list)
 # print(duck_citibike_connect.execute(f"DESCRIBE {list_of_tables['2024']}").fetchall())
 
-print(duck_citibike_connect.execute(
-    f"SELECT * FROM {list_of_tables['2024']} LIMIT 10"
-).df())
+# print(duck_citibike_connect.execute(
+#     f"SELECT * FROM {list_of_tables['2024']} LIMIT 10"
+# ).df())
 
 #CLOSE DATABASE
 duck_citibike_connect.close()
