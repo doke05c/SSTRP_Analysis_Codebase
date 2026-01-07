476c6b720b22ffef931cb2bf7b81504123407fc4
David Oke <davidokenyc@gmail.com>
2026-01-07 11:50:04 -0500

    casted string values with commas for travel times to integers (why were they ever strings with commas to begin with?????????)

--- ems_response_analysis.py
+++ ems_response_analysis.py
@@ -14,7 +14,7 @@ duck_ems_connect = duckdb.connect(database="ems.duckdb")
 #MAKE NAME FOR SQL/DUCKDB TABLE (makes port from previous analysis more convenient)
 table_name = "EMS_TABLE"
 
-#CHECK IF PARQUET HAS ALREADY BEEN CREATED, IF NOT, MAKE IT:
+#CHECK IF PARQUET HAS ALREADY BEEN CREATED, IF NOT, MAKE IT, AND THE TABLE:
 parquet_path = Path("ems_response_since_2015.parquet")
 csv_path = Path("ems_response_since_2015.csv")
 
@@ -29,16 +29,44 @@ if (not (parquet_path.exists())):
         ) TO '{parquet_path}' (FORMAT PARQUET);
     """)
 
+    #CREATE DUCKDB TABLE
+    duck_ems_connect.execute(f"""
+        CREATE TABLE IF NOT EXISTS {table_name} AS 
+        SELECT *
+        FROM read_parquet('{parquet_path}')
+    """)
 
-# IF ALREADY EXISTS, BUT DATA IS OLD, USER RESPONSIBILITY TO REPLACE
-duck_ems_connect.execute(f"""
-    CREATE TABLE IF NOT EXISTS {table_name} AS 
-    SELECT *
-    FROM read_parquet('{parquet_path}')
-""")
+    #RECLASSIFY TRAVEL TIMES AS INTEGERS
+    duck_ems_connect.execute(f"""
+    ALTER TABLE {table_name}
+    ADD COLUMN INCIDENT_TRAVEL_TM_SECONDS_QY_INT INTEGER;
+    """)
+
+    duck_ems_connect.execute(f"""
+    ALTER TABLE {table_name}
+    ADD COLUMN INCIDENT_RESPONSE_SECONDS_QY_INT INTEGER;
+    """)
+
+    duck_ems_connect.execute(f"""
+    UPDATE {table_name}
+    SET
+        INCIDENT_TRAVEL_TM_SECONDS_QY_INT =
+            TRY_CAST(REPLACE(INCIDENT_TRAVEL_TM_SECONDS_QY, ',', '') AS INTEGER),
+        INCIDENT_RESPONSE_SECONDS_QY_INT =
+            TRY_CAST(REPLACE(INCIDENT_RESPONSE_SECONDS_QY, ',', '') AS INTEGER);
+    """)
 
 #ACTUAL ANALYSIS BEGINS HERE
-#lorem ipsum dolom sit amor
+
+for year in list_of_years:
+    summary_results = duck_ems_connect.execute(f"""
+        SELECT INCIDENT_TRAVEL_TM_SECONDS_QY_INT
+        FROM {table_name}
+        LIMIT 5
+    """).fetchall()
+
+for row in summary_results:
+    print(row)
 
 #CLOSE DATABASE
 duck_ems_connect.close()
