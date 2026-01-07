c272b5ae77607b7d61adc5c19968563069fada01
David Oke <davidokenyc@gmail.com>
2026-01-07 12:40:40 -0500

    added validity check, as well as average travel time and response time analysis by month/year and area

--- ems_response_analysis.py
+++ ems_response_analysis.py
@@ -5,7 +5,7 @@ from enum import Enum #make enumerated keywords (words with numerical values)
 import pandas as pd #pandas to work with smaller result summary tables
 
 #TEMPLATE TO FILL IN VARIABLE NAMES BY YEAR
-list_of_years = ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025"] #EDIT HERE TO UPDATE THE LIST OF YEARS TO CHECK
+list_of_years = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025] #EDIT HERE TO UPDATE THE LIST OF YEARS TO CHECK
 
 
 #CREATE DATABASE, WILL BE CLOSED AT THE END OF RUN
@@ -58,15 +58,76 @@ if (not (parquet_path.exists())):
 
 #ACTUAL ANALYSIS BEGINS HERE
 
-for year in list_of_years:
-    summary_results = duck_ems_connect.execute(f"""
-        SELECT INCIDENT_TRAVEL_TM_SECONDS_QY_INT
-        FROM {table_name}
-        LIMIT 5
-    """).fetchall()
+valid_check_all_years_info = []
 
-for row in summary_results:
-    print(row)
+# for year in list_of_years:
+
+validity_results = duck_ems_connect.execute(f"""
+    SELECT
+        EXTRACT(YEAR FROM INCIDENT_DATETIME) AS year,
+
+        SUM(CASE
+            WHEN INCIDENT_TRAVEL_TM_SECONDS_QY_INT = 0
+            THEN 1 ELSE 0 END
+        ) AS invalid_travel_time_count,
+
+        SUM(CASE
+            WHEN INCIDENT_RESPONSE_SECONDS_QY_INT = 0
+            THEN 1 ELSE 0 END
+        ) AS invalid_response_time_count,
+
+        COUNT(*) AS total_entries
+
+        FROM {table_name}
+        GROUP BY year
+        ORDER BY year 
+
+""").fetchdf()
+
+validity_results["invalid_travel_time_count_pct"] = validity_results["invalid_travel_time_count"] / validity_results["total_entries"] * 100
+
+print(validity_results)
+
+
+valid_check_all_years_info = []
+
+# for year in list_of_years:
+
+travel_time_results = duck_ems_connect.execute(f"""
+    SELECT
+        EXTRACT(YEAR FROM INCIDENT_DATETIME) AS year,
+        EXTRACT(MONTH FROM INCIDENT_DATETIME) AS month,
+
+        CASE
+            WHEN INCIDENT_DISPATCH_AREA IN ('M1','M2','M3') THEN 'cbd'
+            WHEN INCIDENT_DISPATCH_AREA IN ('M4','M5','M6','M7','M8','M9') THEN 'non_cbd_mnh'
+            ELSE 'other'
+        END AS area_type,
+
+        AVG(
+            CASE
+                WHEN INCIDENT_TRAVEL_TM_SECONDS_QY_INT != 0
+                THEN INCIDENT_TRAVEL_TM_SECONDS_QY_INT
+                ELSE NULL
+            END
+        ) AS average_travel_time,
+
+        AVG(
+            CASE
+                WHEN INCIDENT_RESPONSE_SECONDS_QY_INT != 0
+                THEN INCIDENT_RESPONSE_SECONDS_QY_INT
+                ELSE NULL
+            END
+        ) AS average_response_time
+
+        FROM {table_name}
+        GROUP BY year, month, area_type
+        ORDER BY year, month, area_type;
+""").fetchdf()
+
+
+print(travel_time_results)
+
 
 #CLOSE DATABASE
 duck_ems_connect.close()
