8339d88ba96c521b2a0795410b7f9d85bd9634ea
David Oke <davidokenyc@gmail.com>
2026-03-11 17:51:47 -0400

    started bus analysis plan, will continue on monday on top of efficiency fixes

---
+++ bus_od_analysis.py
@@ -0,0 +1,6 @@
+##PLAN:
+
+#STAGE 1: Join lat/long to a particular CB
+#STAGE 2: Make a boarding matrix and a deboarding matrix for all stops on each CB (how many boards/deboards for CB x to CB y for all (x,y)?)
+#STAGE 3: Make line features between the n and n-1th point starting from 1st-2nd up to n-1th-nth
+#STAGE 4: Port to ArcGIS and make visual on how many people board/deboard/total on bus line (filter by direction?)
